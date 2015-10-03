87606c131c728226abf96dd7f71e660c41845cc5
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-02 23:21:33 -0500

    Getting stream from Twitter now works

--- TwitterStream.py
+++ TwitterStream.py
@@ -4,13 +4,14 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import json
-import sentiment_mod as s
+import urllib
+# import sentiment_mod as s
 
 # Fill these in.
-ckey = ''
-csecret = ''
-atoken = ''
-asecret = ''
+ckey = '' # Manually enter
+csecret = '' # Manually enter
+atoken = '' # Manually enter
+asecret = '' # Manually enter
 
 class listener(StreamListener):
 	
@@ -21,21 +22,58 @@ class listener(StreamListener):
 
 		print(tweet)
 
-		sentiment_value, confidence = s.sentiment(tweet)
+		filtered_tweet = ''
+		for i in tweet:
+			if ord(i) <= 128:
+				filtered_tweet += i
+			# For testing.
+			# else:
+			# 	print(ord(i))
 
-		if confidence * 100 >= 80:
-			output = open('Twitter-out.txt', 'a')
-			output.write(sentiment_value)
-			output.write('\n')
-			output.close()
+		# Testing
+		print()
+		print(filtered_tweet)
+		print()
+
+		# Connect up to text-processing.com -- they will handle the sentiment
+		# analysis.
+		payload = urllib.urlencode({"text": filtered_tweet}) 
+		result = urllib.urlopen("http://text-processing.com/api/sentiment/", payload)
+		# print result.read()
+		# print result.info()
+
+		content = json.load(result)
+
+		print('Overall: ' + str(content['label']))
+
+		pos = content['probability']['pos']
+		neg = content['probability']['neg']
+		neu = content['probability']['neutral']
+
+		print('Pos: ' + str(pos))
+		print('Neg: ' + str(neg))
+		print('Neu: ' + str(neu))
+
+		print()
+
+		# if confidence * 100 >= 80:
+		# 	output = open('Twitter-out.txt', 'a')
+		# 	output.write(sentiment_value)
+		# 	output.write('\n')
+		# 	output.close()
 
 		return True
 
 	def on_error(self, status):
 		print(status)
 
-auth = OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
+oauth = OAuthHandler(ckey, csecret)
+oauth.set_access_token(atoken, asecret)
 
-twitter_stream = Stream(auth, listener())
-twitter_stream.filter(tack=['Trump'])
+twitter_stream = Stream(oauth, listener())
+twitter_stream.filter(track=['Trump'])
+
+
+
+
+
